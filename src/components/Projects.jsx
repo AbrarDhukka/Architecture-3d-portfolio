7561dd9738def3041728c9f39d4eea9451d4347f
AbrarDhukka <abrdhk@gmail.com>
2024-01-07 16:20:34 +0530

    Updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Bunglow1Canvas from "./canvas/Bunglow1";
@@ -24,7 +26,53 @@ const Projects = () => {
         </p>
       </div>
       <div className="w-[46%] h-[550px] bg-slate-100 rounded-3xl">
-        <Bunglow1Canvas></Bunglow1Canvas>
+        {/* <Bunglow1Canvas></Bunglow1Canvas> */}
+
+        <div className="sketchfab-embed-wrapper w-full h-full">
+  <iframe
+    title="Chinese Old Wall Building-Freepoly.org"
+    frameBorder="0"
+    allowFullScreen
+    mozallowfullscreen="true"
+    webkitallowfullscreen="true"
+    allow="autoplay; fullscreen; xr-spatial-tracking"
+    execution-while-out-of-viewport
+    execution-while-not-rendered
+    web-share
+    src="https://sketchfab.com/models/70fda0cf110c4ccbb7de300560fc35a7/embed"
+    className="w-full h-full"
+  ></iframe>
+  <p style={{ fontSize: '13px', fontWeight: 'normal', margin: '5px', color: '#4A4A4A' }}>
+    <a
+      href="https://sketchfab.com/3d-models/chinese-old-wall-building-freepolyorg-70fda0cf110c4ccbb7de300560fc35a7?utm_medium=embed&utm_campaign=share-popup&utm_content=70fda0cf110c4ccbb7de300560fc35a7"
+      target="_blank"
+      rel="nofollow"
+      style={{ fontWeight: 'bold', color: '#1CAAD9' }}
+    >
+      Chinese Old Wall Building-Freepoly.org
+    </a>{' '}
+    by{' '}
+    <a
+      href="https://sketchfab.com/blackrray?utm_medium=embed&utm_campaign=share-popup&utm_content=70fda0cf110c4ccbb7de300560fc35a7"
+      target="_blank"
+      rel="nofollow"
+      style={{ fontWeight: 'bold', color: '#1CAAD9' }}
+    >
+      Freepoly.org
+    </a>{' '}
+    on{' '}
+    <a
+      href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=70fda0cf110c4ccbb7de300560fc35a7"
+      target="_blank"
+      rel="nofollow"
+      style={{ fontWeight: 'bold', color: '#1CAAD9' }}
+    >
+      Sketchfab
+    </a>
+  </p>
+</div>
+
+
         <div className="bg-slate-700 text-white -mt-28 flex items-center flex-col">
         <p className="z-30 font-serif font-bold text-2xl">Modern luxury villa house building</p>
         <a className="cursor-pointer z-30 hover:underline" target="_blank" rel="noreferrer" href="https://sketchfab.com/3d-models/modern-luxury-villa-house-building-2a07a4a90ae54dd6bba3e33862b266ef">Source - Sketchfab</a>
